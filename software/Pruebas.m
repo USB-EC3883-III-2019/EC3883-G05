1a9f010f3a153c34d3d5fb94e1e46da64e437a9d
alvaroanavarro1 <38303261+alvaroanavarro1@users.noreply.github.com>
2019-10-03 21:40:47 -0400

    Update files
    Create "Transformar" function for x and y position. Update  "readSerial" function and first attemp of "initpolarscatter" to plot using polarscatter.

--- software/Pruebas.m
+++ software/Pruebas.m
@@ -1,13 +1,12 @@
 posicion = [0.017 0.087 0.34 0.52 0.17 1.57 1.65 1.83 2.007 2.09 2.26 3.14]; %Genera vector de posicion de valores de 10 
 distancia = randi([0 80],1,12); %Genera vector de distancia aleatorio con max de 80
 polarscatter(posicion,distancia,'filled');
-
-
+[posicionx,posiciony] = transformar(posicion,distancia);
 
 function [posicionx,posiciony] = transformar(posicion, distancia)
 
-posicionx = [distancia*cos(posicion)];
-posiciony = [distancia*cos(posicion)];
+posicionx = distancia.*cos(posicion);
+posiciony = distancia.*sin(posicion);
 end
 
 
