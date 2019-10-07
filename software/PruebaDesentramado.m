88c381aa8d087d0ca7230855a9e70a3acb906d77
alvaroanavarro1 <38303261+alvaroanavarro1@users.noreply.github.com>
2019-10-07 18:06:54 -0400

    Create Unpackfunction test, Data for testing and updates

---
+++ software/PruebaDesentramado.m
@@ -0,0 +1,24 @@
+databyte = int8(ans);
+start = find(databyte > 0, 1);
+syncData = databyte(start:end-mod((512-start),4)-1);
+syncData = typecast(syncData, 'uint8');
+blocksize = length(syncData)/4;
+byte0 = syncData(1:4:end);
+byte1 = syncData(2:4:end);
+byte2 = syncData(3:4:end);
+byte3 = syncData(4:4:end);
+Mask_posicion(1:blocksize,1) = uint8(63);
+Mask_sonar_l(1:blocksize,1) = uint8(127);
+Mask_sonar_h(1:blocksize,1) = uint8(96);
+Mask_lidar_l(1:blocksize,1) = uint8(31);
+Mask_lidar_h(1:blocksize,1) = uint8(127);
+
+posicion_d = bitand(byte0, Mask_posicion);
+sonar_l = bitand(byte1, Mask_sonar_l);
+sonar_h = bitand(byte2, Mask_sonar_h);
+lidar_l = bitand(byte2, Mask_lidar_l);
+lidar_h = bitand(byte3, Mask_lidar_h);
+
+sonar = uint16(sonar_h)*4 + uint16(sonar_l);
+lidar = uint16(lidar_h)*32 + uint16(lidar_l);
+
--- software/Pruebas.m
+++ software/Pruebas.m
@@ -2,7 +2,7 @@ posicion = [0.017 0.087 0.34 0.52 0.17 1.57 1.65 1.83 2.007 2.09 2.26 3.14]; %Ge
 distancia = randi([0 80],1,12); %Genera vector de distancia aleatorio con max de 80
 polarscatter(posicion,distancia,'filled');
 [posicionx,posiciony] = transformar(posicion,distancia);
-
+128
 function [posicionx,posiciony] = transformar(posicion, distancia)
 
 posicionx = distancia.*cos(posicion);
