c7237b2e353da337f2d97c43d7cf6d464da8741a
AMCA15 <38790021+AMCA15@users.noreply.github.com>
2019-10-12 13:08:21 -0400

    Create a prototype for plot the data

--- software/PruebaDesentramado.m
+++ software/PruebaDesentramado.m
@@ -2,34 +2,40 @@
 DataTest = importdata('DataTest.mat');
 
 % Random data
-%PositionRAW = [[0:40] [40:-1:0]];
-%LidarRAW = randi([512, 4095], 1, length(PositionRAW));
-%SonarRAW = randi([0, 512], 1, length(PositionRAW));
+PositionRAW = [[0:24] [24:-1:-24] [-24:0]];
+LidarRAW = randi([512, 4095], 1, length(PositionRAW));
+SonarRAW = randi([0, 512], 1, length(PositionRAW));
 
 % Unpack and convert data
-[PositionRAW, LidarRAW, SonarRAW] = unpack(DataTest);
+%[PositionRAW, LidarRAW, SonarRAW] = unpack(DataTest);
 [Position, Lidar, Sonar] = ConvertData(PositionRAW, LidarRAW, SonarRAW);
 
 
 % Create polaraxes object
 pax = polaraxes;
 
-% First plot and get the chart line object for Lidar and Sonar
-LidarPlot = polarscatter(pax, Position, Lidar, 'filled');
-hold on;
-SonarPlot = polarscatter(pax, Position, Sonar, 'filled');
-legend('Lidar','Sonar');
-
 % Customize the polar axes
+pax.NextPlot = 'add';
 pax.ThetaZeroLocation = 'top';
 pax.ThetaDir = 'clockwise';
 pax.ThetaTickLabel = {'0', '30', '60', '90', '120', '150', '180', '-150', '-120', '-90', '-60', '-30'};
 pax.RLim = [0 200];
 pax.RTick = 0:50:200;
+pax.ThetaMinorGrid = 'on';
+
+% First plot and get the chart line object for Lidar and Sonar
+LidarPlot = polarscatter(pax, Position, Lidar, 'filled');
+SonarPlot = polarscatter(pax, Position, Sonar, 'filled');
+RadarField = polarhistogram(pax, repelem(0.1,200), 'NumBins', 2, 'BinWidth', pi()/48);
+legend('Lidar','Sonar');
+
 
 % To update the plot...
-LidarPlot.ThetaData = linspace(0,2*pi(),20);   % Update the theta
-SonarPlot.ThetaData = linspace(0,2*pi(),20);
+for i = 1:length(Position)
+    LidarPlot.ThetaData = Position(i);   % Update the theta
+    SonarPlot.ThetaData = Position(i);
 
-LidarPlot.RData = linspace(1,80,20);           % Update the Lidar data
-SonarPlot.RData = linspace(150,50,20);         % Update the Sonar data
+    LidarPlot.RData = Lidar(i);          % Update the Lidar data
+    SonarPlot.RData = Sonar(i);          % Update the Sonar data
+    pause(0.1);
+end
