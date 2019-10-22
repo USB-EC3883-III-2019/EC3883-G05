d1a0395e1da01445725d239631da3bf3776efb37
AMCA15 <38790021+AMCA15@users.noreply.github.com>
2019-10-22 18:47:49 -0400

    Change Sonar/Lidar scale and fix negative position/steps issue

--- software/ConvertData.m
+++ software/ConvertData.m
@@ -6,16 +6,17 @@ function [Position, Lidar, Sonar] = ConvertData(PositionRAW, LidarRAW, SonarRAW)
 % Position conversion
 TotalSteps = 48;
 PositionScale = (2*pi()/TotalSteps);
-Position = PositionScale*PositionRAW;
+Position = round(PositionScale*PositionRAW, 3);
 
 % Lidar conversion
 LidarPoly = [7.67526029129288 -108.134109245746 644.115225669854 -2113.71833279663 4175.24292278666 -5097.1909611203 3802.7451305835 -1649.81901283364 366.038219660162];
-LidarScale = 3/4096;
+LidarScale = 3/4096*1.5;
+disp(LidarScale*LidarRAW);
 LidarEval = polyval(LidarPoly, LidarScale*LidarRAW);
-Lidar = LidarEval*100;
+Lidar = round(LidarEval, 2);
 
 % Sonar conversion
 SonarScale = 122.0703125/58.30;
-Sonar = SonarScale*SonarRAW;
+Sonar = round(SonarScale*SonarRAW, 2);
 end
 
